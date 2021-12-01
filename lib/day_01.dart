71949526066cbb9757213bb8856f340256ab5034
David Öhman <dohman_92@hotmail.com>
2021-12-01 19:44:39 +0100

    Solve day 01

---
+++ lib/day_01.dart
@@ -0,0 +1,36 @@
+import 'reader.dart';
+
+void main() async {
+  part01();
+  part02();
+}
+
+Future<void> part01() async {
+  final input = await readFile('01');
+  final depths = input.map((e) => int.parse(e)).toList();
+  var increments = 0;
+
+  for (var i = 1; i < input.length; i++) {
+    if (depths[i] > depths[i - 1]) {
+      increments++;
+    }
+  }
+
+  print('D01 P01: $increments'); // Correct answer: 1665
+}
+
+Future<void> part02() async {
+  final input = await readFile('01');
+  final depths = input.map((e) => int.parse(e)).toList();
+  var increments = 0;
+
+  for (var i = 0; i < input.length - 3; i++) {
+    final sum = depths[i] + depths[i + 1] + depths[i + 2];
+    final sum2 = depths[i + 1] + depths[i + 2] + depths[i + 3];
+    if (sum2 > sum) {
+      increments++;
+    }
+  }
+
+  print('D01 P02: $increments'); // Correct answer: 1702
+}
