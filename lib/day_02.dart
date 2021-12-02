9153194c3e824b9e7f1c008a59b47b19040c5ed1
David Öhman <dohman_92@hotmail.com>
2021-12-02 17:53:53 +0100

    Solve day 02

---
+++ lib/day_02.dart
@@ -0,0 +1,62 @@
+import 'reader.dart';
+
+void main() async {
+  part01();
+  part02();
+}
+
+Future<void> part01() async {
+  final input = await readFile('02');
+  var horizontal = 0;
+  var depth = 0;
+
+  for (final command in input) {
+    final direction = command.split(' ').first;
+    final units = int.parse(command.split(' ').last);
+
+    switch (direction) {
+      case 'forward':
+        horizontal += units;
+        break;
+      case 'down':
+        depth += units;
+        break;
+      case 'up':
+        depth -= units;
+        break;
+    }
+  }
+
+  final res = horizontal * depth;
+
+  print('D02 P01: $res'); // Correct answer: 2073315
+}
+
+Future<void> part02() async {
+  final input = await readFile('02');
+  var horizontal = 0;
+  var depth = 0;
+  var aim = 0;
+
+  for (final command in input) {
+    final direction = command.split(' ').first;
+    final units = int.parse(command.split(' ').last);
+
+    switch (direction) {
+      case 'forward':
+        horizontal += units;
+        depth += (aim * units);
+        break;
+      case 'down':
+        aim += units;
+        break;
+      case 'up':
+        aim -= units;
+        break;
+    }
+  }
+
+  final res = horizontal * depth;
+
+  print('D02 P02: $res'); // Correct answer: 1840311528
+}
